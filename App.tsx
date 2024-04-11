6af5770e993eb3fa67ea0ecb8f863cf1de5c49c7
lmsand <lmsand99@icloud.com>
2024-04-11 14:02:55 -0500

    imported auth ui screens

--- App.tsx
+++ App.tsx
@@ -1,14 +1,11 @@
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 import 'react-native-gesture-handler';
-import Navigation from "./src/navigation";
-import {
-  withAuthenticator,
-  useAuthenticator
-} from '@aws-amplify/ui-react-native';
-
-import { Amplify } from 'aws-amplify';
+import Navigation from './src/navigation';
+import {withAuthenticator, ThemeProvider} from '@aws-amplify/ui-react-native';
+import {Amplify} from 'aws-amplify';
 import amplifyconfig from './src/amplifyconfiguration.json';
 Amplify.configure(amplifyconfig);
+import colors from './src/theme/colors';
 
 // Amplify.configure(amplifyconfig)
 
@@ -16,13 +13,15 @@ Amplify.configure(amplifyconfig);
   /* <StatusBar style="auto" /> */
 }
 
+
+
 const App = () => {
   return (
-   <SafeAreaProvider>
-    <Navigation />
-   </SafeAreaProvider>
+      <SafeAreaProvider>
+        <Navigation />
+      </SafeAreaProvider>
   );
 };
 
-
-export default withAuthenticator(App);
+// export default withAuthenticator(App);
+export default App;
--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,11 +1,12 @@
-import { NavigationContainer, LinkingOptions } from "@react-navigation/native";
-import CommentsScreen from "../screens/CommentsScreen";
-import { Text } from "react-native";
+import {NavigationContainer, LinkingOptions} from '@react-navigation/native';
+import CommentsScreen from '../screens/CommentsScreen';
+import {Text} from 'react-native';
+import AuthStackNavigator from './AuthStackNavigator';
 
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import BottomTabNavigator from "./BottomTabNavigator";
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import BottomTabNavigator from './BottomTabNavigator';
 
-import { RootNavigatorParamList } from "../types/navigation";
+import {RootNavigatorParamList} from '../types/navigation';
 
 import * as Linking from 'expo-linking';
 
@@ -20,44 +21,44 @@ const linking: LinkingOptions<RootNavigatorParamList> = {
   config: {
     initialRouteName: 'Home',
     screens: {
-      Comments: "comments",
+      Comments: 'comments',
       Home: {
         screens: {
           HomeStack: {
             initialRouteName: 'Feed',
             screens: {
-              UserProfile: 'user/:userId'
-            }
-          }
-        }
-      }
-    }
-  }
+              UserProfile: 'user/:userId',
+            },
+          },
+        },
+      },
+    },
+  },
 };
 
-
 const Navigation = () => {
-
-
   return (
     <NavigationContainer linking={linking} fallback={<Text>Loading...</Text>}>
       <Stack.Navigator
-        initialRouteName="Home"
-        screenOptions={{ headerShown: true }}
-      >
-
-        <Stack.Screen name="Home" component={BottomTabNavigator} options={{ headerShown: false}} />
+        initialRouteName="Auth"
+        screenOptions={{headerShown: true}}>
 
         <Stack.Screen
-          name="Comments"
-          component={CommentsScreen}
+          name="Auth"
+          component={AuthStackNavigator}
+          options={{headerShown: false}}
         />
 
+        <Stack.Screen
+          name="Home"
+          component={BottomTabNavigator}
+          options={{headerShown: false}}
+        />
 
+        <Stack.Screen name="Comments" component={CommentsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
-
 
 export default Navigation;
