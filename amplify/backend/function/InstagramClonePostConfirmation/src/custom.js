44f606647fe9dba3c00d967fba80790a90143fe7
lmsand <lmsand99@icloud.com>
2024-04-15 22:21:10 -0500

    runtime changed to 16 and redeploy

--- amplify/backend/function/InstagramClonePostConfirmation/src/custom.js
+++ amplify/backend/function/InstagramClonePostConfirmation/src/custom.js
@@ -77,3 +77,78 @@ exports.handler = async (event, context) => {
 
   return event;
 };
+
+// const { DynamoDBClient, GetCommand, PutCommand } = require("@aws-sdk/client-dynamodb");
+// const client = new DynamoDBClient({ region: "us-east-1" }); // replace 'us-east-1' with your desired AWS region
+
+// const env = process.env.ENV;
+// const AppsyncID = process.env.API_INSTAGRAMCLONE_GRAPHQLAPIIDOUTPUT;
+// const TableName = `User-${AppsyncID}-${env}`;
+
+// const userExists = async (id) => {
+//   const params = {
+//     TableName,
+//     Key: { id: { S: id } },
+//   };
+
+//   try {
+//     const { Item } = await client.send(new GetCommand(params));
+//     return !!Item; // if Item is a truthy value
+//   } catch (e) {
+//     console.log(e);
+//     return false;
+//   }
+// };
+
+// const saveUser = async (user) => {
+//   const date = new Date();
+//   const dateStr = date.toISOString();
+//   const timestamp = date.getTime();
+
+//   const Item = {
+//     ...user,
+//     __typename: { S: "User" },
+//     createdAt: { S: dateStr },
+//     updatedAt: { S: dateStr },
+//   };
+
+//   const params = {
+//     TableName,
+//     Item,
+//   };
+
+//   try {
+//     await client.send(new PutCommand(params));
+//   } catch (e) {
+//     console.log(e);
+//   }
+// };
+
+// exports.handler = async (event, context) => {
+//   console.log("lambda function is working");
+//   console.log(event);
+
+//   if (!event?.request?.userAttributes) {
+//     console.log("No user data available");
+//     return;
+//   }
+
+//   const { sub, name, email } = event.request.userAttributes; // {sub, email, name}
+
+//   const newUser = {
+//     id: sub,
+//     name,
+//     email,
+//   };
+
+//   // check if user already exists
+//   if (!(await userExists(newUser.id))) {
+//     // if not, save user to database
+//     await saveUser(newUser);
+//     console.log(`User ${newUser.id} has been saved to the database`);
+//   } else {
+//     console.log(`User ${newUser.id} already exists`);
+//   }
+
+//   return event;
+// };
